78f36b15419118501b849f382a76fe4f3bb2f2d4
fergone03 <114108805+fergone03@users.noreply.github.com>
2024-10-16 00:33:42 +0200

    1

--- casinopage/src/App.jsx
+++ casinopage/src/App.jsx
@@ -12,11 +12,11 @@ function App() {
         <h2 className='nombre-empresa'>FortuneFlare</h2>
         </div>
       <nav>
-        <a href="" className="nav-link">HomePage</a>
-        <a href="" className="nav-link">Slots</a>
-        <a href="" className="nav-link">Ruleta</a>
-        <a href="" className="nav-link">BlackJack</a>
-        <a href="" className="nav-link">Todos</a>
+        <a href="./App.jsx" className="nav-link">HomePage</a>
+        <a href="./components/Slots.jsx" className="nav-link">Slots</a>
+        <a href="./components/Ruleta.jsx" className="nav-link">Ruleta</a>
+        <a href="./components/Blackjack.jsx" className="nav-link">BlackJack</a>
+        <a href="./components/AllGames.jsx" className="nav-link">Todos</a>
       </nav>
     </header>
     </>
