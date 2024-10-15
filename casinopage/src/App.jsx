21c8975a1a8fdf27f981a79f8238bc44b6bf5ea1
fergone03 <114108805+fergone03@users.noreply.github.com>
2024-10-15 23:33:41 +0200

    1

---
+++ casinopage/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import casinoLogo from './assets/logo.svg'
+
+function App() {
+
+  return (
+    <>
+    <header>
+      <div className='logoC'>
+        <img src={casinoLogo}/>
+        </div>
+        <nav>
+          a.
+        </nav>
+    </header>
+    </>
+  )
+}
+
+export default App
